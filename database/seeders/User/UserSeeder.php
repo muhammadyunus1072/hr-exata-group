8ec7369e4834b8f0b5dff43cbe9b45c4a4bf43f9
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-03-31 14:05:08 +0700

    udpate

--- database/seeders/User/UserSeeder.php
+++ database/seeders/User/UserSeeder.php
@@ -19,16 +19,6 @@ class UserSeeder extends Seeder
             'username' => "admin",
             'email' => "admin@gmail.com",
             'password' => Hash::make("123"),
-
-            'nama_karyawan' => '-',
-            'nomor_identitas' => '-',
-            'tempat_lahir' => '-',
-            'tanggal_lahir' => now(),
-            'jenis_kelamin' => '-',
-            'agama' => '-',
-            'status_perkawinan' => '-',
-            'pendidikan_terakhir' => '-',
-            'jenis_apresiasi' => User::APPRECIATION_JALAN_JALAN,
         ]);
 
         $user->assignRole('Super Admin');
